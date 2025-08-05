09d2e826e0998009742cd01870870543cd74a228
SRKads1998 <kadonoffs@gmail.com>
2025-08-05 12:14:01 -0700

    made a change

--- Derivitive Practice.R
+++ Derivitive Practice.R
@@ -26,3 +26,13 @@ my_derivative <-D(expr =expression(3.1 * X^4 -28 * x), name = "x")
 gz <- expression(2*z^3 - 10.5*z^2 + 4.1)
 dg_dz <- D(expr = gz, name = "z")
 dg_dz
+
+# example 2
+dt_dy <- D(expr = expression((2*y^3+1)^4 -8*y^3), name = "y")
+dt_dy
+
+# find the slope of T(y) at a range of values
+y <- seq(from = -0.4, to =2.0, by = 0.1)
+
+# evaluate the slope
+eval(dt_dy)
